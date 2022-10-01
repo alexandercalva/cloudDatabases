bf01a1d3fbb2effd3b3e9fc08a60761ef24b54b2
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2022-09-30 20:50:20 -0500

    Class and function

--- index.py
+++ index.py
@@ -1,28 +1,56 @@
+#from typing_extensions import Self
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication("https://fir-python-ce049-default-rtdb.firebaseio.com/",None)
 
-# Variables
+#class
 
-datos = {
-    'id': '1',
-    'Name' : 'Alexander',
-    'LastName': 'Calva',
-    'Phone': '0983545654',
-    'Mail': 'prueba@gmail.com',
-    'Address': 'Urb. Limonar',
-    'Job': 'Carpinter' 
-}
+class persona:
+    def __init__(self, name, lastname, phone, mail, password, address, job):
+        self.name = name
+        self.lastname = lastname
+        self.phone = phone
+        self.mail = mail
+        self.password = password
+        self.address = address
+        self.job = job
+        
 
+    def __str__(self):
+        return 'Name: {}\nLastname: {}\nPhone: {}\nMail: {}\naddress: {}\nJob: {}'.format(self.name, self.lastname, self.phone, self.mail, self.address, self.job)
+    
+    def IntroInfo(self):
+        self.name = input("Name: ")
+        self.lastname = input("Lastname: ")
+        self.phone = input("Phone: ")
+        self.mail = input("Mail: ")
+        self.password = input("Password: ")
+        self.address = input("Address: ")
+        self.Job = input("Job: ")
+
+        datos = {
+            'Name' : self.name,
+            'LastName': self.lastname,
+            'Phone': self.phone,
+            'Mail': self.mail,
+            'Address': self.address,
+            'Job': self.job,
+            'Passsword': self.password 
+        }
+        return datos
+
+personaF = persona("Alex", "Calva", 983880592, "correo@gmail.com", "hola","urba", "developer")
+ansPost = firebase.post('/tutorial_firebase/data_post',personaF.IntroInfo())
+#print(personaF)
 # Add registers to Database(post)
-ansPost = firebase.post('/tutorial_firebase/data_post',datos)
+
 
 # Read data from firebase
 ansGet = firebase.get('/tutorial_firebase/data_post', '')
 print(ansGet)
 
 #Edit data from firebase
-firebase.put('/tutorial_firebase/data_post/-NCkPLmQatOI0hdefvLh', 'id', '2')
+#firebase.put('/tutorial_firebase/data_post/-NCkPLmQatOI0hdefvLh', 'id', '2')
 
 # Delete data from firebase
-firebase.delete('/tutorial_firebase/data_post', '-NCkQKqjEs7SOWm3OSsx')
+#firebase.delete('/tutorial_firebase/data_post', '-NCkQKqjEs7SOWm3OSsx')
