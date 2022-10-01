877bd0c98165640ff72db7614630eab65ee87422
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2022-10-01 16:10:34 -0500

    CRUD finish

--- index.py
+++ index.py
@@ -7,7 +7,6 @@ import sys
 firebase = firebase.FirebaseApplication("https://fir-python-ce049-default-rtdb.firebaseio.com/",None)
 
 #class
-
 class persona:
            
     # Enter information in each field in the database
@@ -40,9 +39,10 @@ class persona:
             'Passsword': hashed.decode(encoding),
             'Experience': experience
         }
-        ansPost = firebase.post('/tutorial_firebase/data_post',datos)
-        #return datos
-    
+        idUserFb = datos['Id']
+        idUserFb = str(idUserFb)
+        ansPost = firebase.post('/tutorial_firebase/data_post', datos)
+           
     # Read data from firebase
     def ShowData(self):
         ansGet = firebase.get('/tutorial_firebase/data_post', '')
@@ -50,17 +50,55 @@ class persona:
 
     #Edit data from firebase
     def EditData(self):
-        firebase.put('/tutorial_firebase/data_post/-NCkPLmQatOI0hdefvLh', 'id', '2')   
+        idUserFirebase = input("Enter the user idFirebase:  ")
+        keyDatabase = input("Enter the key to edit: ")
+        valueDatabase = input("Enter the value to change: ")
+        try:   
+            firebase.put('/tutorial_firebase/data_post/' +idUserFirebase , keyDatabase, valueDatabase)
+        except:
+            print("Error. Please, enter the information correctly")
+    
+    # Delete data form firebase
+    def DeleteData(self):
+        idUserFirebase = input("Enter the user idFirebase:  ")
+        try:
+            firebase.delete('/tutorial_firebase/data_post',idUserFirebase)
+            print("Information was deleted correctly")
+        except:
+             print("Error. Please, enter the information correctly")
 
-# Instance of persona class
-personaFirestore = persona()
+# Menu 
+def menu():
+    opc = int(input("Menu \n" +
+    "1. Register \n" + 
+    "2. Show \n" + 
+    "3. Update \n" + 
+    "4. Delete \n" + 
+    "5. Exit \n" +
+    "Choose an option: \n"))
+    
+    return opc
 
-# Introduce information
-personaFirestore.IntroInfo()
-# Show all information
-personaFirestore.ShowData()
+def main(opcion):
+    # Instance of persona class
+    personaFirestore = persona()
+    
+    while opcion != 5:
+       
+        if opcion == 1:
+            # Introduce information
+            personaFirestore.IntroInfo()
+        if opcion == 2:
+            # Show all information
+            personaFirestore.ShowData()
+        if opcion == 3:
+            # Edit user
+            personaFirestore.EditData()
+        if opcion == 4:
+            # Delete data from firebase
+            personaFirestore.DeleteData()
+        opcion = menu()
+opcion = menu()
+main(opcion)
 
 
-
-# Delete data from firebase
-#firebase.delete('/tutorial_firebase/data_post', '-NCkQKqjEs7SOWm3OSsx')
